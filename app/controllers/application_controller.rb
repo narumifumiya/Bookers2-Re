57ee933b8469fd316233698051586523e75f82b8
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-05 10:24:56 +0000

    flash,errorメッセージの文字色変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:top, :about]
 
+  add_flash_types :success, :info, :warning, :danger
+
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
   end
